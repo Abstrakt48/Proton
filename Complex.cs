529b03b93164431a62bdb2cecbae085c24acf41a
Oliver Hughes <78424173+Abstrakt48@users.noreply.github.com>
2021-08-19 16:24:35 +0100

    Add files via upload

---
+++ Complex.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace ProtonEngine
+{
+	public class Complex {
+	
+		public float real;
+		public float im;
+		
+		public Complex(float real, float im)
+		{
+			this.real = real;
+			this.im   = im;
+		}
+		
+		public void add(Complex c)
+		{
+			real += c.real;
+			im   += c.im;
+		}
+		
+		public void sub(Complex c)
+		{
+			real -= c.real;
+			im   -= c.im;
+		}
+		
+		public void mul(Complex c)
+		{
+			real = real * c.real - im * c.im;
+			im   = real * c.im   + im * c.real;
+		}
+		
+		public void mul(float i)
+		{
+			real *= i;
+			im   *= i;
+		}
+
+		public float getReal() 
+		{
+			return real;
+		}
+
+		public void setReal(float real) 
+		{
+			this.real = real;
+		}
+
+		public float getIm() 
+		{
+			return im;
+		}
+
+		public void setIm(float im) 
+		{
+			this.im = im;
+		}
+
+	}
+}
